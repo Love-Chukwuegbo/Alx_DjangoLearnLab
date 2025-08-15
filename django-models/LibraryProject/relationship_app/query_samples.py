facdfedcef2d1cf1c0330ed87bf8d63aa66b4eee
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-08-15 18:40:22 +0100

    Added django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,31 +1,30 @@
-from . import models
+from .models import Library, Librarian,Author,Book
 
 # Query all books by a specific author
-achebe =models.Author(name = "Chinua Achebe")
+achebe =Author(name = "Chinua Achebe")
 achebe.save()
 
-Books =[models.Book(title= "Things fall apart", author= achebe),
-        models.Book(title= "No longer at ease", author= achebe)]
+Books =[Book(title= "Things fall apart", author= achebe),
+        Book(title= "No longer at ease", author= achebe)]
 
-new_book =models.Book(title= "The beautiful ones", author= achebe)
+new_book =Book(title= "The beautiful ones", author= achebe)
 
 
-models.Book.objects.bulk_create(Books)
+Book.objects.bulk_create(Books)
 new_book.save()
 
-books_by_achebe= models.Book.objects.filter(author = achebe)
+books_by_achebe= Book.objects.filter(author = achebe)
 
 
 # List all books in a library
-futo= models.Library(name="Futo Library")
+futo= Library(name="Futo Library")
 
-futo.save()
-models.Library.objects.get(name= "Futo Library" )
+futo.save().Library.objects.get(name= "Futo Library" )
 
-models.Library.books.all()
+Library.books.all()
 
 #Retrieve the librarian for a library
-librarian1=models.Librarian(name="Njoku",  library =futo)
+librarian1=Librarian(name="Njoku",  library =futo)
 librarian1.save()
 
-models.Librarian.objects.filter(library =futo)
+Librarian.objects.filter(library =futo)
