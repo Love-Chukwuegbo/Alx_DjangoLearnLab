afbb55d6c93dc794c6f658d3c453a20a27a726db
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-08-16 16:16:59 +0100

    Editted the relationship app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import  list_books
 from .views import LibraryDetailView
 
-urlpatterns = [path("all/", list_books, name="list_books"),
-               path("library/", LibraryDetailView.as_view(), name="library_detail")
+urlpatterns = [
+    path("all/", list_books, name="list_books"),
+    path("library/", LibraryDetailView.as_view(), name="library_detail")
                
 ]
+
