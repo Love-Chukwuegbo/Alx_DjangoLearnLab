5e11f91569c9a80049e68f106cd8b0e77a356878
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-08-27 12:16:34 +0100

    Editted the views

--- django-models/LibraryProject/relationship_app/helpers.py
+++ django-models/LibraryProject/relationship_app/helpers.py
@@ -4,7 +4,7 @@ def is_admin(user):
     return user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
 def is_librarian(user):
-    return user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "Member"
+    return user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
 
 def is_member(user):
-    return user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+    return user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "Member"
